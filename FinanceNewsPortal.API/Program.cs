9f86d7d1cebee70284ac6285f64778f9d75847e5
mardanniel <mardannielabarondoginturo@gmail.com>
2023-04-05 09:40:27 +0800

    Add services

--- FinanceNewsPortal.API/Program.cs
+++ FinanceNewsPortal.API/Program.cs
@@ -1,11 +1,15 @@
+using FinanceNewsPortal.API.Repository;
+using FinanceNewsPortal.API.Repository.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHttpClient();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IRatesRepository, RatesAPIRepository>();
 
 var app = builder.Build();
 
