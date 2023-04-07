79f154e167168c69345c4c5512079e57f2190b4f
mardanniel <mardannielabarondoginturo@gmail.com>
2023-04-07 10:51:21 +0800

    remove unused code

--- FinanceNewsPortal.Web/wwwroot/js/site.js
+++ FinanceNewsPortal.Web/wwwroot/js/site.js
@@ -2,8 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$( document ).ready( function () {
-  $( '#sidebarCollapse' ).on( 'click', function () {
-    $( '#sidebar' ).toggleClass( 'active' );
-  } );
-} );
