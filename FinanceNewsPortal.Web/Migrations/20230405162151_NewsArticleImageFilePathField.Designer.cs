fa186aeedba0ec31a333f336decde2ede043b14c
mardanniel <mardannielabarondoginturo@gmail.com>
2023-04-07 10:46:41 +0800

    Update migrations

--- FinanceNewsPortal.Web/Migrations/20230405162151_NewsArticleImageFilePathField.Designer.cs
+++ FinanceNewsPortal.Web/Migrations/20230405162151_NewsArticleImageFilePathField.Designer.cs
@@ -4,6 +4,7 @@ using FinanceNewsPortal.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinanceNewsPortal.Web.Migrations
 {
     [DbContext(typeof(FinanceNewsPortalDbContext))]
-    partial class FinanceNewsPortalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230405162151_NewsArticleImageFilePathField")]
+    partial class NewsArticleImageFilePathField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
