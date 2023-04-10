95e901f6df0de544abe09766d083b3dd15143e64
mardanniel <mardannielabarondoginturo@gmail.com>
2023-04-11 04:54:22 +0800

    Add Models

---
+++ FinanceNewsPortal.API/Models/NewsArticleType.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace FinanceNewsPortal.API.Models
+{
+    public class NewsArticleType
+    {
+        public Guid Id { get; set; }
+
+        public Guid NewsArticleTagId { get; set; }
+
+        [ValidateNever]
+        public NewsArticleTag NewsArticleTag { get; set; }
+
+        public Guid NewsArticleId { get; set; }
+
+        [ValidateNever]
+        public NewsArticle NewsArticle { get; set; }
+    }
+}
