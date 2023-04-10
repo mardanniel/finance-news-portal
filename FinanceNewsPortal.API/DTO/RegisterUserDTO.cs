5d7a48bffa3fa0475f2b58c5d2112b843b9346b5
mardanniel <mardannielabarondoginturo@gmail.com>
2023-04-11 04:54:01 +0800

    Add DTOs

---
+++ FinanceNewsPortal.API/DTO/RegisterUserDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace FinanceNewsPortal.API.DTO
+{
+    public class RegisterUserDTO
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirm password doesnt match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public char Gender { get; set; }
+    }
+}
