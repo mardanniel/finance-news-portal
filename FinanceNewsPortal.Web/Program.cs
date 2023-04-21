a3b8a530a78c6b208e9fc75607281340fd9f9bbf
mardanniel <mardannielabarondoginturo@gmail.com>
2023-04-21 10:43:07 +0800

    add company repository

--- FinanceNewsPortal.Web/Program.cs
+++ FinanceNewsPortal.Web/Program.cs
@@ -1,8 +1,11 @@
 using FinanceNewsPortal.Web.Data;
 using FinanceNewsPortal.Web.Helper;
 using FinanceNewsPortal.Web.Models;
-using FinanceNewsPortal.Web.Repository;
-using FinanceNewsPortal.Web.Repository.Contracts;
+using FinanceNewsPortal.Web.Repository.AdminRepository;
+using FinanceNewsPortal.Web.Repository.CompanyRepository;
+using FinanceNewsPortal.Web.Repository.NewsArticleRepository;
+using FinanceNewsPortal.Web.Repository.RatesRepository;
+using FinanceNewsPortal.Web.Repository.UserRepository;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +20,7 @@ builder.Services.AddScoped<INewsArticlesRepository, NewsArticlesRepository>();
 builder.Services.AddScoped<IRatesRepository, RatesAPIRepository>();
 builder.Services.AddScoped<IAdminRepository, AdminRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
 builder.Services.AddScoped<FileUpload>();
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
